f966a2b438c75da4241431a08dc0fa0185d6495f
WidyaX1900 <tuderangga93@gmail,com>
2024-12-01 21:36:22 +0800

    Change the application title

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -9,7 +9,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>School Data Management Application</title>
+    <title>School Data Management App</title>
     <link rel="stylesheet" href="{{ config('app.asset_url') }}bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="{{ config('app.asset_url') }}css/app.css?v={{ $version }}">
     <script src="{{ config('app.asset_url') }}js/jquery-3.7.1.min.js"></script>
