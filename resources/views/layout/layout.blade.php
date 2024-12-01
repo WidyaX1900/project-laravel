2d0114ed309a2a1d84895312140a8883b6409314
WidyaX1900 <tuderangga93@gmail,com>
2024-12-01 18:21:36 +0800

    Use cache robusting to handle static asset

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -1,3 +1,7 @@
+@php
+    $version = time();
+@endphp
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -6,15 +10,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>School Data Management Application</title>
-    @vite(['resources/css/app.css', 'resources/js/jquery-3.7.1.min.js'])
-    <link rel="stylesheet" href="{{ config('app.url') }}resources/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="{{ config('app.asset_url') }}bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="{{ config('app.asset_url') }}css/app.css?v={{ $version }}">
+    <script src="{{ config('app.asset_url') }}js/jquery-3.7.1.min.js"></script>
 </head>
 
 <body>    
     @yield('navbar')
     @yield('content')
-    <script src="{{ config('app.url') }}resources/bootstrap/js/bootstrap.min.js"></script>
-    @vite(['resources/js/app.js'])
+    <script src="{{ config('app.asset_url') }}bootstrap/js/bootstrap.min.js"></script>
+    <script src="{{ config('app.asset_url') }}js/app.js?v={{ $version }}"></script>
 </body>
 
 </html>
